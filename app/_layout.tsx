fdf10a5788f1b60b2805352c36346e322a25a51e
flopez008 <flpaul6000@hotmail.com>
2026-05-16 20:16:59 -0500

    Integrantes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Stack } from "expo-router";
+import { Slot } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
+import { initDatabase } from '../domain/data/local/connection';
 
 export default function RootLayout() {
-  //testDatabaseConnection();
-  return <Stack />;
+  const [dbReady, setDbReady] = useState(false);
+
+  useEffect(() => {
+    initDatabase()
+      .then(() => setDbReady(true))
+      .catch((error) => console.error('❌ Error DB:', error));
+  }, []);
+
+  if (!dbReady) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  return <Slot />;
 }
